72a1961e2d5b162db0daefd1818efbb509d54b29
Contykpo <samu.1furlanich@gmail.com>
2024-09-18 22:37:16 -0300

    Implmeented ContactForm component
    -Added a Contact form powered by Basin form builder since it has a free plan and this website will be hosted on a static site.
    -New Header.razor nav bar.
    -Reworked Project.razor structure.
    -Updated Home.razor for new nav bar and contact information.

---
+++ Models/EmailForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace samuelfurlanich.github.io.Models
+{
+    public class EmailForm
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Message is required.")]
+        public string Message { get; set; } = string.Empty;
+    }
+
+}
